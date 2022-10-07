19b73fd7faa9142e08d43724b6f2bd62037acb05
Carlosjw <carlos.ti.infor@gmail.com>
2022-10-07 00:03:05 -0300

    journey Beginning

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,13 @@
+export function RepositoryItem({repository}) {
+    return (
+        <li>
+            <strong>{repository.name}</strong>
+            <p>Forms in React</p>
+
+            <a href="">
+                Acessar repositório
+            </a>
+        </li>
+    )
+
+}
